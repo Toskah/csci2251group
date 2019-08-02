8e830f0f09eca70cf4f62e1df483fb96989922cf
= <=>
2019-08-01 19:24:20 -0600

    wut

--- src/main/java/driver/Driver.java
+++ src/main/java/driver/Driver.java
@@ -1,7 +1,35 @@
 package driver;
+
+import dao.DAOFactory;
+import dao.PropertyDAO;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.logging.Logger;
+
+
+/**
+ * Author @Dexter Elliott
+ */
 // create instances of DAO Packages
 // send to web client
 public class Driver {
+    String OwnerID = "ID";
+    PropertyDAO DAO = DAOFactory.create(PropertyDAO.class);
+    try{
+        List<PropertyDAO.PropertyBaseData> Properties = DAO.listAllPropertiesByOwner(OwnerID);
+    } catch SQLException e
+
+
+    public Driver() throws SQLException {
+
+    }
+    private static Logger getLogger(){
+        return Logger.getLogger(Driver.class.getName());
+    }
+
+    private void sendInfo(){
+
+    }
 
 
 }
