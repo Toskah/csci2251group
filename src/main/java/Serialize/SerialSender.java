959b27505557880e51e8767466b2f83518b6a541
= <=>
2019-08-06 21:06:20 -0600

    Serial Receiver Update

--- src/main/java/Serialize/SerialSender.java
+++ src/main/java/Serialize/SerialSender.java
@@ -1,83 +1,24 @@
 package Serialize;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 import java.io.IOException;
 import java.io.ObjectOutputStream;
-import java.io.Serializable;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
 /**
- * Demo for sending a serialized object through the network.
+ * Handles Sending to website
  *
- * @author Kenneth Ingham
+ * @author Dexter Elliott
  */
 public class SerialSender {
-    /**
-     * Connect to the specified server.
-     *
-     * @param host the host name to connect to
-     * @param port the port on the host
-     * @return a set up Socket connected to the server
-     */
-    private static Socket ServerConnect(String host, int port) {
-        // Open connection to server
-        Socket server = null;
-        try {
-            server = new Socket(host, port);
-        } catch (UnknownHostException e) {
-            System.err.println("Received an Unknown host exception for host '"
-                    + host + "': " + e.getMessage());
-            System.exit(1);
-        } catch (IOException e) {
-            System.err.println("Received an I/O exception for server host '"
-                    + host + "' on port " + port + ": " + e.getMessage());
-            System.exit(1);
+
+    public SerialSender(){
+        try{
+            Socket socket = new Socket("Local Host", 8082);
+            ObjectOutputStream OOS = new ObjectOutputStream(socket.getOutputStream());
+        }catch (IOException e){
+            e.getMessage().trim();
         }
-        System.out.println("Connected to " + host + ":" + port);
-        return server;
     }
 
-    public static void main(String args[]) {
-        if (args.length != 3) {
-            System.err.println("Invalid argument count: " + args.length);
-            System.err.println("Usage: java SerialSender hostname port string");
-            System.exit(1);
-        }
-        String host = args[0];
-        Integer port = Integer.parseInt(args[1]);
-        SerializedClass data = new SerializedClass(args[2]);
-
-        Socket server = ServerConnect(host, port);
-        ObjectOutputStream serverObj = null;
-        try {
-            serverObj = new ObjectOutputStream(server.getOutputStream());
-        } catch (IOException e) {
-            System.err.println("Received an I/O exception while getting "
-                    + " OutputStream for server" + e.getMessage());
-            System.exit(1);
-        }
-
-        // Send the object.  Remember to flush the stream.
-        try {
-            serverObj.writeObject(data);
-        } catch (IOException e) {
-            System.err.println("Received an I/O exception while writing "
-                    + " object " + e.getMessage());
-            System.exit(1);
-        }
-        try {
-            serverObj.flush();
-        } catch (IOException e) {
-            System.err.println("Received an I/O exception while flushing "
-                    + " object " + e.getMessage());
-            System.exit(1);
-        }
-        System.out.println("Sent " + data.toString());
-    }
 }
